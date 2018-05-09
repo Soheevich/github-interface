c3608af9a7f359d6b46962ab82debe381baf3026
Andrei F <soheevich@gmail.com>
2018-05-09 18:08:35 +0300

    Edit reducer

--- src/store/reducers/filterReducer.js
+++ src/store/reducers/filterReducer.js
@@ -1,22 +1,50 @@
 import * as actionsTypes from '../actions/actionTypes';
 
 const initialState = {
-  issues: null
+  author: null,
+  repositories: null,
+  selectedRepository: null,
+  issues: null,
+  loading: false
 };
 
-const filterReducer = (state = initialState, actionTypes) => {
-  switch (actionTypes) {
+const filterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.SELECT_AUTHOR:
+      return {
+        ...state,
+        author: action.author
+      };
+    case actionTypes.FETCH_REPOSITORIES_START:
+      return {
+        ...state,
+        loading: true
+      };
+    case actionTypes.FETCH_REPOSITORIES_SUCCESS:
+      return {
+        ...state,
+        repositories: action.repositories,
+        loading: false
+      };
+    case actionTypes.FETCH_REPOSITORIES_FAIL:
+      return {
+        ...state,
+        loading: false
+      };
     case actionTypes.FETCH_ISSUES_START:
       return {
-        ...state
+        ...state,
+        loading: true
       };
     case actionTypes.FETCH_ISSUES_SUCCESS:
       return {
-        ...state
+        ...state,
+        loading: false
       };
     case actionTypes.FETCH_ISSUES_FAIL:
       return {
-        ...state
+        ...state,
+        loading: false
       };
     default:
       return {
