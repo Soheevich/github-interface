051a8b3bc40aac9cdbbb664f0aac4683add8a3f0
Andrei F <soheevich@gmail.com>
2018-05-09 16:55:20 +0300

    Add components and containers

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import './App.css';
+import Layout from './components/Layout/Layout';
+import IssuesFinder from './containers/IssuesFinder/IssuesFinder';
+import IssueDetails from './containers/IssueDetails/IssueDetails';
+
+
+const App = (props) => (
+  <Fragment>
+    <Layout>
+      <Switch>
+        <Route path="/issue" component={IssueDetails}/>
+        <Route path="/" exact component={IssuesFinder} />
+      </Switch>
+    </Layout>
+  </Fragment>
+);
 
 export default App;
