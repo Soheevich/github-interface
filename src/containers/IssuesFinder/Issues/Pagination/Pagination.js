c2a02165b3a23e932f9f09a94c453fea55f52853
Andrei F <soheevich@gmail.com>
2018-05-11 14:43:59 +0300

    Add previous and next buttons to pagination

--- src/containers/IssuesFinder/Issues/Pagination/Pagination.js
+++ src/containers/IssuesFinder/Issues/Pagination/Pagination.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './Pagination.css';
 
-const Pagination = (props) => {
-  return (
-    <div>
-      <p>Current Page {props.list.currentPage}, total pages {props.list.totalPages}</p>
-    </div>
-  );
+class Pagination extends Component {
+  state = {
+    currentPage: this.props.list.currentPage,
+    totalPages: this.props.list.totalPages
+  }
+
+  render() {
+    let previousPageClasses = ['PaginationButton', 'PreviousPage'];
+    let nextPageClasses = ['PaginationButton', 'NextPage'];
+  
+    if (this.state.currentPage === 1) {
+      previousPageClasses.push('Disabled');
+    }
+    if (this.state.currentPage === this.state.totalPages) {
+      nextPageClasses.push('Disabled');
+    }
+  
+    return (
+      <div className="Pagination">
+        <span className={previousPageClasses.join(' ')}>Previous</span>
+        <span className={nextPageClasses.join(' ')}>Next</span>
+      </div>
+    );
+  }
 };
 
 export default Pagination;
